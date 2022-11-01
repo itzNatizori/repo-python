ac46a8c6e9f5fc558f066b0a5d5a57e3aa86218b
Ziran <84247135+itzNatizori@users.noreply.github.com>
2022-11-01 13:31:35 -0300

    Add files via upload

---
+++ primeiroscomandos.py
@@ -0,0 +1,16 @@
+print("Olá, Mundo")
+print(9 + 9)
+print("8" + "9")
+print("Olá", 5)
+
+# Variaveis 
+nome = "Arthur"
+idade = 14
+peso = 78.9
+print(nome, idade, peso)
+
+#Interação
+nome2 =input("Qual é seu nome?")
+idade2 =input("Qual é sua idade?")
+peso2 =input("Quanto você pesa?")
+print(nome2, idade2, peso2)
