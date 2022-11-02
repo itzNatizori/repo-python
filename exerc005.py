594d455118e80a082b564a73d92ab0b4af82cc1a
Ziran <84247135+itzNatizori@users.noreply.github.com>
2022-11-02 12:44:37 -0300

    Update exerc005.py

--- exerc005.py
+++ exerc005.py
@@ -1,20 +1,12 @@
-# bool e float conversão
-n =bool(input("Digite um valor: ")) # se nada for digitado, ele retorna o valor "false", caso contrario, "true".
-print(n)
 
-m =float(input("Converter valor, digite um número: ")) # retornara um número com ponto flutuante
-print(m)
-
-#-------- é numerico? teste ----------
 numb =input("Digite algo: ")
-print(numb.isnumeric())
+print("É númerico?", numb.isnumeric())
 print(type(numb))
-#-------- são letras? teste ----------
-test =input("Digite: ")
-print(numb.isalpha())
-print(type(test))
-#-------- é alfa numerico? teste ----------
-test2 =input("Digite alf: ")
-print(test2.isalnum())
-print(type(test2))
 
+print("São Letras?", numb.isalpha())
+
+print("É alfa numerico?", numb.isalnum())
+
+print("Está em maiúsculas?", numb.isupper())
+
+print("Está em minúsculas?", numb.islower())
